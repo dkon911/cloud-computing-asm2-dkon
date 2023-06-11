2e1332c3b8caa3870a027bba88e4ec1ece37c9ac
Duy Cong <congngo147@gmail.com>
2023-06-11 14:48:26 +0700

    create route app/ controller

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const handlebars = require('express-handlebars');
 const port = 3000;
+const route = require('./routes/index');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended: true}));
@@ -10,21 +11,10 @@ app.use(express.json());
 
 //template engine
 app.engine('hbs', handlebars.engine({ defaultLayout: 'main', extname: '.hbs'}));
+app.set('views', path.join(__dirname, 'resources/views'));
 app.set('view engine', 'hbs');
 
-app.set('views', path.join(__dirname, 'resources/views'));
+//routes init
+route(app);
 
-app.get('/', (req, res) => {
-    res.render('home');
-});
-
-app.get('/news', (req, res) => {
-    res.render('news');
-});
-
-app.get('/search', (req, res) => {
-    res.render('search');
-})  
-
-app.use;
 app.listen(port,()=>console.log(`listening on port`+port));
