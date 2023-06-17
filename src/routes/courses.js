cf7ace2d2c72d934fbcbcd3e5e03faf5d03e8c41
Duy Cong <congngo147@gmail.com>
2023-06-17 07:11:31 +0700

    fix Create add footer config route

--- src/routes/courses.js
+++ src/routes/courses.js
@@ -3,10 +3,10 @@ const router = express.Router();
 
 const courseController = require('../app/controllers/CourseController');
 
-router.get('/create', courseController.create);
+router.get('/create', courseController.create);//get Create form
 router.post('/store', courseController.store);
 
-router.delete('/:id', courseController.destroy);
+router.delete('/:id', courseController.destroy);//delete
 router.get('/:id/edit', courseController.edit);
 router.put('/:id', courseController.update); // update
 router.get('/:slug', courseController.show);
